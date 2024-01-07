9d6e8a2d6cc69d0071bda2a4c9fa49d21be274eb
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2024-01-07 19:00:56 +0700

    create events page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,120 @@
-import Image from 'next/image'
+// import Image from 'next/image'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Event Asinkron Indonesia',
+  description: 'Kumpulan workshop bahasa indonesia react.js, next.js dan remix run',
+  openGraph: {
+    title: 'Event Asinkron Indonesia',
+    description: 'Kumpulan workshop bahasa indonesia react.js, next.js dan remix run',
+    locale: 'id_ID',
+    type: 'website',
+    images: [
+      {
+        url: '/assets/img/facebook-meta-remix-next-step.png',
+      },
+    ],
+    // image: '/assets/img/logo-asinkron-favicon.png',
+  }
+}
+
+function formatDate(date: Date, type: string): string {
+  date = new Date(date);
+  let formatter = null;
+  if (type === 'hour') {
+    formatter = new Intl.DateTimeFormat('id-ID', { hour: '2-digit', minute: '2-digit' });
+  } else if (type === 'day') {
+    formatter = new Intl.DateTimeFormat('en-US', { day: '2-digit' });
+  } else {
+    formatter = new Intl.DateTimeFormat('en-US', { month: 'short' });
+  }
+  return formatter.format(date);
+}
 
 export default function Home() {
+  const events: Array<any> = [{
+    status: true,
+    place: 'Zoom',
+    url: '/details/remix-next-step',
+    dateTime: '2024-01-13 09:30:00',
+    title: 'Remix, Next Step: Membuat Notes App',
+    description: 'Disini kita akan membahasa cara kerja remix, lalu belajar membuat notes app dengan database JSON.'
+  }, {
+    status: true,
+    place: 'Zoom',
+    url: '/details/remix-next-step',
+    dateTime: '2024-01-01 09:30:00',
+    title: 'Remix, Next Step: Membuat Notes App',
+    description: 'Disini kita akan membahasa cara kerja remix, lalu belajar membuat notes app dengan database JSON.'
+  }];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <div className={`flex flex-col gap-y-[30px]`}>
+      {events.map(event => (
+          <div className={`flex flex-wrap bg-white dark:bg-gray-800 py-7 pr-7 relative`}>
+            { !event.status || new Date(event.dateTime) < new Date() ? (
+              <div className={`disable-event absolute top-0	left-0 w-full h-full`}/>
+            ) : (
+              ''
+            )}
+            <div className={`w-20 text-center md:w-24 lg:w-[140px]`}>
+              <time className={`flex flex-col items-center text-xs font-bold uppercase md:text-base`} dateTime={event.dateTime}>
+                <span className={`text-accent`}>
+                  {formatDate(event.dateTime, 'month')}
+                </span>
+                <span className={`text-2xl tracking-tighter md:text-[34px] md:leading-none text-primary dark:text-white`}>
+                  {formatDate(event.dateTime, 'day')}
+                </span>
+              </time>
+            </div>
+            <div className={`flex flex-1 flex-wrap items-center`}>
+            <div className={`basis-full lg:basis-7/12 lg:pr-8`}>
+              <h4 className={`mb-3 text-lg font-bold leading-tight tracking-tight md:mb-1 md:text-1.5xl md:leading-tight lg:leading-none`}>
+                <a className={`text-primary dark:text-white transition-colors hover:text-accent dark:hover:text-accent`} href={event.url}>
+                  {event.title}
+                </a>
+              </h4>
+              <div className={`text-sm md:text-base lg:tracking-tight xl:text-lg xl:leading-8`}>
+                <p className={`leading-tight`}>
+                  {event.description}
+                </p>
+              </div>
+            </div>
+            <div className={`basis-full lg:basis-5/12`}>
+              <ul className={`flex flex-wrap gap-y-5 pt-7 lg:pt-0`}>
+                <li className={`flex basis-full gap-x-4 leading-tight sm:basis-1/2 md:basis-1/3 lg:basis-7/12 xl:basis-2/3`}>
+                  <div className={`lg:pt-1`}>
+                    <svg role="img" className={`h-6 w-6 fill-primary dark:fill-white`}>
+                      <use xlinkHref="assets/img/icons.svg#pin"></use>
+                    </svg>
+                  </div>
+                  <div>
+                    <h5 className={`font-bold tracking-tight text-primary dark:text-white md:text-lg md:leading-tight lg:pb-[2px]`}>
+                      {event.place}
+                    </h5>
+                    <span className={`text-xs lg:text-sm`}>Where</span>
+                  </div>
+                </li>
+                <li className={`flex basis-full gap-x-4 leading-tight sm:basis-1/2 md:basis-1/3 lg:basis-5/12 xl:basis-1/3`}>
+                  <div className={`lg:pt-1`}>
+                    <svg role="img" className={`h-6 w-6 fill-primary dark:fill-white`}>
+                      <use xlinkHref="assets/img/icons.svg#clock"></use>
+                    </svg>
+                  </div>
+                  <div>
+                    <h5 className={`font-bold tracking-tight text-primary dark:text-white md:text-lg md:leading-tight lg:pb-[2px]`}>
+                      {formatDate(event.dateTime, 'hour')}
+                    </h5>
+                    <span className={`text-xs lg:text-sm`}>Time</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          </div>
+      ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,24 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'purple': '#4126b2',
+      'gray-dark': '#15171a',
+      'gray': '#979797',
+      'gray-light': '#979797',
+      'gray-lighter': '#fcfcfc',
+      'white': '#ffffff',
     },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    }
+    // extend: {
+    //   backgroundImage: {
+    //     'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+    //     'gradient-conic':
+    //       'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    //   },
+    // },
   },
   plugins: [],
 }
